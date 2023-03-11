1cd949edf6025ec6ac5f4f65e66ef94821f2e374
KurogiShota <85198635+KurogiShota@users.noreply.github.com>
2023-03-11 00:10:45 +0000

    csv add

--- testt.py
+++ testt.py
@@ -3,7 +3,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+df = pd.read_csv("test.csv")
+df = df.fillna(0)
 
+print(df.loc[0])
 df = pd.DataFrame([0,1,2,3,5])
 print("ok")
 
